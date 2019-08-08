28c67729dd9dd52993320b709dcc41f74c833ffb
Jonathan Dorety <kyullog@gmail.com>
2019-08-07 19:47:28 -0700

    Complete module 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -1,6 +1,6 @@
 """
 Python makes performing file I/O simple. Take a look
-at how to read and write to files here: 
+at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
@@ -9,10 +9,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
-
+file = open("foo.txt", "r")
+print(file.read())
+file.close()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+new_file = open("bar.txt", "w")
+new_file.write("zim zim zam \n")
+new_file.write("zem zem zum \n")
+new_file.write("zom zom zam \n")
+new_file.close()
+
+read_file = open("bar.txt", "r")
+print(read_file.read())
+read_file.close()
