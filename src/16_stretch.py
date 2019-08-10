2b958fc9db0c7503a3fb0952258bb6a866d13b45
Jonathan Dorety <kyullog@gmail.com>
2019-08-10 11:19:24 -0700

    Add error handling for incorrect input

--- src/16_stretch.py
+++ src/16_stretch.py
@@ -1,28 +1,28 @@
-import math
 from sys import argv
 
+integer = 0
 
 def find_prime(num):
-  int_num = int(num)
-  range_list = [i for i in range(2, int_num + 1)]
-  print(range_list)
+  number = num
+  range_list = [i for i in range(2, number + 1)]
   multiples = []
   for i in range_list:
     for j in range_list:
       multiples.append(i * j)
-  print(multiples)
-  if (int_num in multiples):
+  if (number in multiples):
     return False
   else:
     return True
 
 
 if len(argv) == 2:
-  print("that's right!")
-  if (find_prime(argv[1]) == True):
-    print(f"{argv[1]} is a prime!")
-  else:
-    print(f"{argv[1]} is not a prime!")
+  try:
+    integer = int(argv[1])
+    if (find_prime(integer) == True):
+      print(f"{integer} is a prime!")
+    else:
+      print(f"{integer} is not a prime!")
+  except ValueError:
+    print("Please input an integer")
 else:
   print("Please pass an integer on the command line")
-
